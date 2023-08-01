38f715bc2f8b501faab52ab31faf56f946da2945
Michel de Jong (C) <SPLUNKCORP\mdejong@a-27g9g960b0cxf.splunkcorp.com>
2023-08-01 15:54:34 +0200

    Bug fixes disabling script

--- disabling_savedsearches.py
+++ disabling_savedsearches.py
@@ -1,25 +1,23 @@
 import os
 import shutil
+import sys
 
 __name__ = "disabling_savedsearches.py"
 __author__ = "Michel de Jong"
 
-# Path to the directory containing Splunk apps
-apps_dir = input("Enter the path to the directory containing Splunk apps: \n")
-
-# Create the "apps_ss_disabled" directory to copy apps
-disabled_ss_dir = os.path.join(os.path.dirname(apps_dir), "apps_ss_disabled")
-
-def process_file(file_path):
+# Function to process the savedsearches.conf file
+def process_file(file_path, debug=False):
     try:
         # Read the savedsearches.conf file
         with open(file_path, "r") as f:
             lines = f.readlines()
+
         # Loop over the lines in the file
         for i in range(len(lines)):
             line = lines[i].strip()
             # Check if the line starts a new stanza
-            if line.startswith("["):
+            if line.startswith("[") and not line.startswith("[default]") and not line.endswith("\\"):
+                stanza_name = line[1:-1]  # Extract the stanza name without brackets
                 # Check if the stanza has a disabled statement
                 has_disabled = False
                 for j in range(i+1, len(lines)):
@@ -32,37 +30,60 @@ def process_file(file_path):
                         if "false" in next_line or "0" in next_line:
                             # Disable the stanza by changing the statement to "disabled = 1"
                             lines[j] = "disabled = 1\n"
+                            if debug:
+                                print(f"DEBUG: Disabled Stanza '{stanza_name}' in {file_path}")
+                        break
+                    if next_line.startswith("search") or next_line.startswith("|"):
                         break
                 # If the stanza doesn't have a disabled statement, add one
                 if not has_disabled:
                     lines.insert(i+1, "disabled = 1\n")
+                    if debug:
+                        print(f"DEBUG: Inserted 'disabled = 1' for Stanza '{stanza_name}' in {file_path}")
+
         # Write the modified savedsearches.conf file
         with open(file_path, "w") as f:
             f.writelines(lines)
-            print(f"INFO: Disabled {file_path}")
+            if debug:
+                print(f"DEBUG: Modified {file_path}")
+            else:
+                print(f"INFO: Disabled saved searches in {file_path}")
+
     except PermissionError:
         print(f"ERROR: Permission denied: {file_path}")
 
-# Copy the apps to apps_ss_disabled directory
-while os.path.exists(disabled_ss_dir):
-    decision = input(f"Destination directory {disabled_ss_dir} already exist. Proceed and overwrite? (y/n): \n")
-    if decision.lower() == "y":
-        shutil.rmtree(disabled_ss_dir)
-        break
-    elif decision.lower() == "n":
-        print("Exiting the script")
-        exit(0)
-shutil.copytree(apps_dir, disabled_ss_dir)
+if __name__ == "disabling_savedsearches.py":
+    # Parse command-line arguments
+    args = sys.argv[1:]
+    debug_mode = "-debug" in args
 
-# Loop over all directories in the apps_ss_disabled directory
-for app_name in os.listdir(disabled_ss_dir):
-    app_dir = os.path.join(disabled_ss_dir, app_name)
-    if os.path.isdir(app_dir):
-        # Look for savedsearches.conf in the app/default directory
-        savedsearches_conf_path = os.path.join(app_dir, "default", "savedsearches.conf")
-        if os.path.isfile(savedsearches_conf_path):
-            process_file(savedsearches_conf_path)
-        # Look for savedsearches.conf in the app/local directory
-        savedsearches_conf_path = os.path.join(app_dir, "local", "savedsearches.conf")
-        if os.path.isfile(savedsearches_conf_path):
-            process_file(savedsearches_conf_path)
+    # Path to the directory containing Splunk apps
+    apps_dir = input("Enter the path to the directory containing Splunk apps: \n")
+
+    # Create the "apps_ss_disabled" directory to copy apps
+    disabled_ss_dir = os.path.join(os.path.dirname(apps_dir), "apps_ss_disabled")
+
+    # Copy the apps to apps_ss_disabled directory
+    while os.path.exists(disabled_ss_dir):
+        decision = input(f"Destination directory {disabled_ss_dir} already exists. Proceed and overwrite? (y/n): \n")
+        if decision.lower() == "y":
+            shutil.rmtree(disabled_ss_dir)
+            break
+        elif decision.lower() == "n":
+            print("Exiting the script")
+            exit(0)
+    print("Copying the apps directory to apps_ss_disabled")
+    shutil.copytree(apps_dir, disabled_ss_dir)
+
+    # Loop over all directories in the apps_ss_disabled directory
+    for app_name in os.listdir(disabled_ss_dir):
+        app_dir = os.path.join(disabled_ss_dir, app_name)
+        if os.path.isdir(app_dir):
+            # Look for savedsearches.conf in the app/default directory
+            savedsearches_conf_path = os.path.join(app_dir, "default", "savedsearches.conf")
+            if os.path.isfile(savedsearches_conf_path):
+                process_file(savedsearches_conf_path, debug=debug_mode)
+            # Look for savedsearches.conf in the app/local directory
+            savedsearches_conf_path = os.path.join(app_dir, "local", "savedsearches.conf")
+            if os.path.isfile(savedsearches_conf_path):
+                process_file(savedsearches_conf_path, debug=debug_mode)
